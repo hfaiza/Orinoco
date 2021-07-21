40e8bb9460445e5d3d7b7102156ada2db2226f75
Faiza Hamidi <hamidi.faiza@gmail.com>
2021-07-21 17:06:29 +0200

    Optimisation du code

--- js/index.js
+++ js/index.js
@@ -1,32 +1,38 @@
-// Pour envoyer une requête HTTP de type GET au service web afin de récupérer les données :
-fetch("http://localhost:3000/api/cameras")
-  .then((response) => response.json()) // Fonction appelée pour convertir le résultat de la requête au format JSON
-  .then((cameras) => showCameras(cameras)) // Pour récupérer le contenu de la réponse
-  .catch((error) => alert(error)); // Fonction appelée si une erreur survient lors de la requête
+const showCameras = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/cameras"); // Pour envoyer une requête HTTP de type GET au service web afin de récupérer les données
+    const data = await response.json(); // Pour convertir le résultat de la requête au format JSON
 
-// Pour afficher tous les articles disponibles à la vente :
-const showCameras = (cameras) => {
-  const camerasList = document.querySelector(".cameralist");
-  // Pour exécuter la fonction sur chaque élément du tableau (création d'un <article> pour chaque caméra) :
-  cameras.forEach((camera) => {
-    const cameraArticle = document.createElement("article");
-    cameraArticle.classList.add("border", "border-5", "border-primary", "gap");
-    // Pour insérer dynamiquement le contenu de l'article (image,  nom et  prix de la caméra) :
-    cameraArticle.innerHTML = `<a href="item.html?id=${camera._id}">
-                                 <img
-                                  src="${camera.imageUrl}"
-                                  alt="Caméra vintage ${camera.name}."
-                                 />
-                                 <div class="d-flex justify-content-between bg-primary p-2">
-                                   <h3>
-                                   ${camera.name}
-                                   </h3>
-                                   <p>
-                                   ${camera.price / 100} €
-                                   </p>
-                                 </div>
-                               </a>`;
-    // Pour insérer l'article en tant que dernier enfant du parent <div> :
-    camerasList.append(cameraArticle);
-  });
+    // Pour ajouter au DOM tous les articles disponibles à la vente :
+    const camerasList = document.querySelector(".cameralist");
+    data.forEach((camera) => {
+      const cameraArticle = document.createElement("article");
+      cameraArticle.classList.add(
+        "border",
+        "border-5",
+        "border-primary",
+        "gap"
+      );
+      cameraArticle.innerHTML = `<a href="item.html?id=${camera._id}">
+                                   <img
+                                    src="${camera.imageUrl}"
+                                    alt="Caméra vintage ${camera.name}."
+                                   />
+                                   <div class="d-flex justify-content-between bg-primary p-2">
+                                     <h3>
+                                     ${camera.name}
+                                     </h3>
+                                     <p>
+                                     ${camera.price / 100} €
+                                     </p>
+                                   </div>
+                                 </a>`;
+      camerasList.append(cameraArticle);
+    });
+  } catch (error) {
+    alert(error); // Bloc exécuté si une erreur survient lors de la requête
+  }
 };
+
+// Appel de la fonction :
+showCameras();
